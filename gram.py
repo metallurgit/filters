09ee078087b60a55638efde989b6666ee1cf04b2
metallurgeek <metallurgeek@gmail.com>
2017-06-19 07:43:59 +0200

    The code is a bit more compact.

--- gram.py
+++ gram.py
@@ -1,11 +1,10 @@
-# gram.py - Metallurgeek - May 2017
+# gram.py - Metallurgeek - June 2017
 import sys
 import argparse
 
 # Parse
 parser = argparse.ArgumentParser(
-description = "count occurrences of all n-grams up to length n.",
-epilog =      "(standard input, one word per line, stop on empty line)")
+description = "count occurrences of all n-grams up to length n.")
 parser.add_argument("-n", type = int, default = 2,
 help =        "1 for monograms, 2 for bigrams (default), 3 for trigrams, etc.")
 parser.add_argument("-m", type = int, default = 1,
@@ -15,12 +14,11 @@ args = parser.parse_args()
 # Prepare
 span = args.n + 1	# the maximal width of n-grams
 freq = {}		# the frequency (count) of n-grams
-line = sys.stdin.readline().strip() # read line by line (for huge files)
 
 # Search
-while line:	# stop on empty line
+for line in sys.stdin:
 
-  maxi = len(line)
+  maxi = len(line) - 1
   
   # Generate {1..n}-grams
   for init in range(maxi):
@@ -33,8 +31,6 @@ while line:	# stop on empty line
         freq[gram] += 1
       else:
         freq[gram] = 1
-
-  line = sys.stdin.readline().strip()	# read line by line
 
 # Output 
 for gram in sorted(freq, key=freq.get, reverse=True):	# by decreasing occurrences
